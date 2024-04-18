02ba44a27cfd254a01442df930e7075f0d636c52
chmadnansultan <info.adnansultan@gmail.com>
2024-04-18 12:23:04 +0500

    Completed the Lab Task along with OOP JavaScript

--- Sessional-Quetion1-and-Question3-Solution/Question1/assets/main.js
+++ Sessional-Quetion1-and-Question3-Solution/Question1/assets/main.js
@@ -1,5 +1,36 @@
-// Define an array to store applicant data
+// Define an array to store applicants outside the class
 let applicants = [];
+
+class ApplicantManager {
+    constructor(cnic, firstName, lastName) {
+        this.cnic = cnic;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Method to save applicant data
+    saveData() {
+        // Create applicant object
+        let applicant = {
+            cnic: this.cnic,
+            firstName: this.firstName,
+            lastName: this.lastName
+        };
+
+        // Add applicant object to the array
+        applicants.push(applicant);
+
+        // Clear input fields
+        document.getElementById("cnic").value = "";
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+
+        alert("Data saved successfully!");
+    }
+}
+
+// Create an instance of the ApplicantManager class
+const applicantManager = new ApplicantManager();
 
 // Function to save applicant data
 function saveData() {
@@ -7,45 +38,25 @@ function saveData() {
     let cnic = document.getElementById("cnic").value;
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
-    
 
-    // Create applicant object
-    let applicant = {
-        firstName: firstName,
-        lastName: lastName,
-        cnic: cnic
-    };
+    // Update properties of applicantManager instance
+    applicantManager.cnic = cnic;
+    applicantManager.firstName = firstName;
+    applicantManager.lastName = lastName;
 
-    // Add applicant object to the array
-    applicants.push(applicant);
-
-    // Clear input fields
-    document.getElementById("cnic").value = "";
-    document.getElementById("firstName").value = "";
-    document.getElementById("lastName").value = "";
-    
-
-    alert("Data saved successfully!");
+    // Save data using method from the class
+    applicantManager.saveData();
 }
+
 
 // Function to print applicant data
 function printData() {
     // Sort applicants array based on last name
     applicants.sort((a, b) => (a.lastName > b.lastName) ? 1 : -1);
 
-    // Display applicant data
-    let output = "<h2>Applicant Data</h2>";
-    output += "<table>";
-    output += "<tr><th>Cnic</th><th>First Name</th><th>Last Name</th></tr>";
-    applicants.forEach(applicant => {
-        output += "<tr>";
-        output += "<td>" + applicant.cnic + "</td>";
-        output += "<td>" + applicant.firstName + "</td>";
-        output += "<td>" + applicant.lastName + "</td>";
-        output += "</tr>";
-    });
-    output += "</table>";
+    // Save applicant data to sessionStorage or localStorage
+    localStorage.setItem("applicants", JSON.stringify(applicants));
 
-    // Display output on the page
-    document.getElementById("output").innerHTML = output;
+    // Redirect to registration page
+    window.location.href = 'registeration.html';
 }
